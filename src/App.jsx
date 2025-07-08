2853ff0b82c5148b071cb3e77c71b1fe82e73e19
manishbisht2000 <manishbisht2000@gmail.com>
2025-07-08 22:34:12 +0530

    Overview page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import OverviewPage from "./pages/OverviewPage"
+import ProductsPage from "./pages/ProductsPage"
 
 function App() {
 
   return (
-    <div className='text-red-500 text-4xl bg-blue-500'>
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
       
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </div>
   )
 }
