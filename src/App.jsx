43c48c1c27fb05905b331d1fe59b16969d3b051c
manishbisht2000 <manishbisht2000@gmail.com>
2025-07-15 23:15:48 +0530

    Orders page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ProductsPage from "./pages/ProductsPage"
 import Sidebar from "./components/Sidebar"
 import UsersPage from "./pages/UsersPage"
 import SalesPage from "./pages/SalesPage"
+import OrdersPage from "./pages/OrdersPage"
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
       </Routes>
     </div>
   )
